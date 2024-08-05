22af2f1cb330519d98b68cc5c1fe5229539a4c7c
Prince Sanguan <princesanguan44@gmail.com>
2024-08-05 22:16:09 +0800

    creating axios in react laravel app

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,20 +1,55 @@
-import React from "react";
+import { React, useRef } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
 
 export default function Signup() {
-    const onSubmit = (ev) => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
+
+    const { setUser, setToken } = useStateContext();
+
+    function onSubmit(ev) {
         ev.preventDefault();
-    };
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        };
+
+        axiosClient.post("/signup", payload).then((data) => {
+            setUser(data.user);
+            setToken(data.token);
+        });
+
+        .catch(err => {
+            const response = err.response;
+            if (response && response.status == 422) {
+              console.log(response.data.errors);
+            }
+        })
+    }
 
     return (
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
                 <form onSubmit={onSubmit} action="">
                     <h1 className="title">Signup for free</h1>
-                    <input type="text" placeholder="Full name" />
-                    <input type="email" placeholder="Email Address" />
-                    <input type="password" placeholder="Password" />
+                    <input ref={nameRef} type="text" placeholder="Full name" />
                     <input
+                        ref={emailRef}
+                        type="email"
+                        placeholder="Email Address"
+                    />
+                    <input
+                        ref={passwordRef}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <input
+                        ref={passwordConfirmationRef}
                         type="password"
                         placeholder="Password Confirmation"
                     />
@@ -28,3 +63,5 @@ export default function Signup() {
         </div>
     );
 }
+
+//1:17:00
